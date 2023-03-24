cb84568d0afd4226e4cd0c3487cc0b6a46ebaa93
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2023-03-24 14:06:57 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -23,3 +23,22 @@ function initListeners() {
 $(document).ready(function () {
   initListeners();
 });
+
+
+
+
+// I created a button.js file
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+const link = document.querySelector(".links a");
+
+//when hamburger is clicked activate mobileMenu function
+hamburger.addEventListener("click", mobileMenu);
+
+//this function makes menu active
+function mobileMenu() {
+  // turns hamburger into X
+  hamburger.classList.toggle("active");
+  // opens up navigation links
+  navMenu.classList.toggle("active");
+}
