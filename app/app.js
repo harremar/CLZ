97987628bb1204024580da9f239cbf5ee15c32b1
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2023-04-15 13:56:42 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -22,6 +22,74 @@ function initListeners() {
   route();
 }
 
+//this is the change day function that is on the Riley TV page.
+function changeday() {
+  const events = [
+    "No Event",
+    "Picture Bingo",
+    "Tech Time with Jacob",
+    "Picture Bingo",
+    "Craft Time with Kim",
+    "Surprise Eggs",
+    "No Event",
+  ];
+  const eventsDes = [
+    "There will be no episode today!",
+    "Ask your child life specialist for today's bingo sheet and call in when you win.",
+    "Learn about a new STEM topic each week with crafts, 3d printing and Minecraft.",
+    "Ask your child life specialist for today's bingo sheet and call in when you win.",
+    "Follow along with the provided craft kit or just watch for fun.",
+    "Call in to pick an egg and win a bag full of prizes!",
+    "There will be no episode today!",
+  ];
+
+  const bg = [
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/trees.jpg)",
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/bingo.jpg)",
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/tech.jpg)",
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/games.jpg)",
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/crafts.jpg)",
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/easter.jpg)",
+    "linear-gradient(to right,rgba(31, 2, 117, 0.9),rgba(195, 0, 255, 0.7)), url(../../css/img/pool.jpg)",
+  ];
+
+  const eventType = document.getElementById("eventType");
+  const eventDesType = document.getElementById("eventDesType");
+  const eventBG = document.getElementById("eventbg");
+
+  const dayref = document.getElementById("day");
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dateToday = new Date();
+  let dayToday = dateToday.getDay();
+
+  //display day of week
+  dayref.innerHTML = `${daysOfWeek[dayToday]}'s Event`;
+
+  const eventDay = events[dayToday];
+  const eventDesDay = eventsDes[dayToday];
+  const eventbgday = bg[dayToday];
+
+  //display the day event
+  eventType.innerHTML = eventDay;
+
+  //display the day event description
+  eventDesType.innerHTML = eventDesDay;
+
+  //change background style
+  eventBG.style.backgroundImage = eventbgday;
+}
+
+//checking every second
+setInterval(changeday, 1000);
+
 $(document).ready(function () {
   initListeners();
 });
